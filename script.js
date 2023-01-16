3593d83957664f13aa91b391eddb45a3acee8f59
giorgidavitoshvili <119691539+giorgidavitoshvili@users.noreply.github.com>
2023-01-16 12:33:10 -0800

    javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+var userName = 'Mamuka';
+let newUser = 'sardioni';
+const maxAge = 50;
+
+
+let age = 20;
+let number = 10;
+console.log(age + number);
+
+let age1 = 20;
+let number1 = 10;
+console.log(age1 - number1);
+
+let age2 = 20;
+let number2 = 10;
+console.log(age2 / number2);
+
+let age3 = 20;
+let number3 = 10;
+console.log(age2 * number2);
+
+const height = 7;
+const width = 5;
+console.log(height * width)
